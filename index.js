8dd810dc08270570a75ba6ca2ca9d53a23d97a11
Dominique Walker <DW629@live.mdx.ac.uk>
2022-02-01 13:53:26 +0000

    cors issue

--- index.js
+++ index.js
@@ -5,6 +5,16 @@ const app = express();
 const cors = require('cors');
 
 
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", req.headers.origin);
+    res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
+
 app.use('/static', express.static(path.join(__dirname, 'public')))
 
 
@@ -22,20 +32,13 @@ const posts = require('./routes/api/posts')
 app.use('/', posts)
 
 
-app.use(cors());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-  });
 
 
 
 
-const port = process.env.PORT || 8000;
-app.listen(port, () => console.log('server started on port 8000'));
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => console.log('server started on port 8080'));
 
 
 
