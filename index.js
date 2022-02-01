8d0f301009c9243ad5aee5bc0009522463e5a658
Dominique Walker <DW629@live.mdx.ac.uk>
2022-02-01 14:14:48 +0000

    push

--- index.js
+++ index.js
@@ -1,17 +1,25 @@
+
 var express = require('express');  
+const cors = require('cors');
+const app = express();
+app.use(cors());
 const bodyParser = require('body-parser')
 const path = require('path')
-const app = express();
-const cors = require('cors');
 
 
-app.use(cors());
+
+
+
+
+
 
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", req.headers.origin);
-    res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
+  router.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "*");
+    res.header("Access-Control-Allow-Headers", "*");
+    next();});
+    
   });
 
 
