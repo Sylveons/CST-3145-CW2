4b472d27eded868286b60e40a640074700408973
Dominique Walker <DW629@live.mdx.ac.uk>
2022-02-03 21:13:32 +0000

    i hope this works

--- index.js
+++ index.js
@@ -4,10 +4,27 @@ const cors = require('cors');
 const app = express();
 app.use(cors());
 const bodyParser = require('body-parser')
-const path = require('path')
+var path = require("path");
+var fs = require("fs");
 
 
-
+app.use(function(req, res, next) {
+    // Uses path.join to find the path where the file should be
+    var filePath = path.join(__dirname,
+    "public"
+    , req.url);
+    // Built-in fs.stat gets info about a file
+    fs.stat(filePath, function(err, fileInfo) {
+    if (err) {
+    next();
+    res.send("file not found!")
+    return;
+    }
+    if (fileInfo.isFile()) res.sendFile(filePath);
+    else next();
+    });
+    });
+  
 
 
 
